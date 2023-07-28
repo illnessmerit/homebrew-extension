64f0edf3e582893922435f8e15f03c4b126c757f
a <a@as-Virtual-Machine.local>
2023-07-28 02:01:29 -0700

    Add TODO comment to format document

--- extension.rb
+++ extension.rb
@@ -1,3 +1,4 @@
+# TODO: Format document
 class Extension < Formula
   url "https://github.com/8ta4/extension/releases/download/v0.1.0/extension.tar.gz"
   sha256 "416a7560b5a32eed676fe82be239fff70c4e679631c3197aeca625a5d6e31f65"
