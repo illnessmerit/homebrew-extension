6ac45593ae97d74bf6336d3c8abe88c463b67989
a <a@as-Virtual-Machine.local>
2023-10-29 01:23:50 -0700

    Delete TODO.txt and format extension.rb

--- extension.rb
+++ extension.rb
@@ -1,9 +1,11 @@
-# TODO: Format document
-class Extension < Formula
-  url "https://github.com/8ta4/extension/releases/download/v0.1.3/extension.tar.gz"
-  sha256 "6a8c7d094c6428dad28f1aa5b05b0df7b4cf76b6f398fd6c42483753123053fa"
+# frozen_string_literal: true
 
-  depends_on "node"
+# This class represents a Homebrew formula for the Extension project.
+class Extension < Formula
+  url 'https://github.com/8ta4/extension/releases/download/v0.1.3/extension.tar.gz'
+  sha256 '6a8c7d094c6428dad28f1aa5b05b0df7b4cf76b6f398fd6c42483753123053fa'
+
+  depends_on 'node'
 
   def install
     # We need to copy the whole directory including output/Main because
@@ -11,7 +13,7 @@ class Extension < Formula
     # problem with dynamic require of 'stream' when using 'bundle-app'.
     # 'spago build' generates 'index.js' in './output/Main/', which needs
     # to be available in the working directory for import.
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"index.js" => "extension"
+    libexec.install Dir['*']
+    bin.install_symlink libexec / 'index.js' => 'extension'
   end
 end
