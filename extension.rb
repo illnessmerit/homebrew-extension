d74d1a06eaca1d061385b74413dbfa58b480f9af
a <a@as-Virtual-Machine.local>
2023-12-16 18:56:57 -0800

    Remove unnecessary blank lines in extension.rb

--- extension.rb
+++ extension.rb
@@ -4,9 +4,7 @@
 class Extension < Formula
   url 'https://github.com/8ta4/extension/releases/download/v0.1.3/extension.tar.gz'
   sha256 '6a8c7d094c6428dad28f1aa5b05b0df7b4cf76b6f398fd6c42483753123053fa'
-
   depends_on 'node'
-
   def install
     # We need to copy the whole directory including output/Main because
     # 'spago build' is being used instead of 'spago bundle-app' due to a
