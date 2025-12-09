34c725026091068ad962d46abbcacba763ccc3f3
a <a@as-Virtual-Machine.local>
2025-12-08 21:40:44 -0800

    Bump up 'extension' tool version and checksum

--- extension.rb
+++ extension.rb
@@ -2,8 +2,8 @@
 
 # This class represents a Homebrew formula for the extension project.
 class Extension < Formula
-  url 'https://github.com/8ta4/extension/releases/download/v0.3.1/extension.tar.gz'
-  sha256 '8d75285dac2f4ee97c3522e03cfb01933b5d94378e7e9e4d6111aad544c387cd'
+  url 'https://github.com/8ta4/extension/releases/download/v0.3.2/extension.tar.gz'
+  sha256 '0e127160dbb6165b7cedfd1a63224a2d1f502f853adb3021705527c00904eb85'
   depends_on 'node'
   def install
     libexec.install Dir['*']
